fed5be5eae56917a1f93855071bd668e8be759b4
Carlos Fontanez <charliefontanezis@gmail.com>
2022-06-28 18:16:32 -0400

    verifying password

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -63,6 +63,13 @@ router.post('/login', (req, res) => {
     // res.json({ user: dbUserData });
     
     // Verify user
+    const validPassword = dbUserData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect password!' });
+      return;
+    }
+
+    res.json({ user: dbUserData, message: 'You are now logged in!' });
 
   });
 });
