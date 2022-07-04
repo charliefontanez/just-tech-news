9aa9de1b864d85e8f904ffab171cf702ee2c5386
Carlos Fontanez <charliefontanezis@gmail.com>
2022-07-04 15:35:14 -0400

    logout button now appears when user is logged in

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -40,11 +40,12 @@ router.get('/:id', (req, res) => {
     where: {
       id: req.params.id
     },
-    attributes: ['id',
-     'post_url',
-     'title',
-     'created_at',
-     [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
     include: [
       {
@@ -66,7 +67,10 @@ router.get('/:id', (req, res) => {
         res.status(404).json({ message: 'No post found with this id' });
         return;
       }
-      res.json(dbPostData);
+
+      const post = dbPostData.get({ plain: true });
+
+      res.render('single-post', { post });
     })
     .catch(err => {
       console.log(err);
@@ -89,12 +93,16 @@ router.post('/', (req, res) => {
 });
 
 router.put('/upvote', (req, res) => {
-  Post.upvote(req.body, { Vote })
-    .then(updatedPostData => res.json(updatedPostData))
-    .catch(err => {
-      console.log(err);
-      res.status(400).json(err);
-    });
+  // make sure the session exists first
+  if (req.session) {
+    // pass session id along with all destructured properties on req.body
+    Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+      .then(updatedVoteData => res.json(updatedVoteData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  }
 });
 
 router.put('/:id', (req, res) => {
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -32,7 +32,10 @@ router.get('/', (req, res) => {
       // pass a single post object into the homepage template
       console.log(dbPostData[0]);
       const posts = dbPostData.map(post => post.get({ plain: true }));
-      res.render('homepage', { posts });
+      res.render('homepage', { 
+        posts,
+        loggedIn: req.session.loggedIn
+      });
     })
     .catch(err => {
       console.log(err);
@@ -47,6 +50,25 @@ router.get('/login', (req, res) => {
   }
 
   res.render('login');
+});
+
+router.get('/post/:id', (req, res) => {
+  const post = {
+    id: 1,
+    post_url: 'https://handlebarsjs.com/guide/',
+    title: 'Handlebars Docs',
+    created_at: new Date(),
+    vote_count: 10,
+    comments: [{}, {}],
+    user: {
+      username: 'test_user'
+    }
+  };
+
+  res.render('single-post', { 
+    post,
+    loggedIn: req.session.loggedIn
+  });
 });
 
 module.exports = router;
